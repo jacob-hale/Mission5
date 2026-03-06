e6e007e9359c60f056a3cffc2f421adbe5cb3f82
Jacob Hale <Jchale23@byui.edu>
2026-03-06 11:37:58 -0700

    setting up the functions before using the json data

--- March/src/App.tsx
+++ March/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import myData from "./CollegeBasketBallTeams.json";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Welcome() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>March Madness is upon us</h1>
+      <h2>View all the Teams and their info below</h2>
     </>
-  )
+  );
 }
 
-export default App
+function Team({
+  school,
+  name,
+  city,
+  state,
+}: {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}) {
+  return (
+    <>
+      <h3>{school}</h3>
+      <h4>{name}</h4>
+      <h4>
+        {city}, {state}
+      </h4>
+    </>
+  );
+}
+
+
+
+function App() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
